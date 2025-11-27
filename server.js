5bc4f421f77c5df4f630907431cfc8d71dab8676
junjhon12 <q.le102100@gmail.com>
2025-11-27 02:54:49 -0500

    fix: update path to serve pages/login/index.html

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(express.static(path.join(__dirnameResolved, 'public')));
 
 
 app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'login', 'login.html'));
+  res.sendFile(path.join(__dirname, 'public','pages', 'login', 'index.html'));
 });
 
 

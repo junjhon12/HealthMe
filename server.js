5381d74cc38436b76e541d325e675e07cad53783
junjhon12 <q.le102100@gmail.com>
2025-11-27 02:50:57 -0500

    fix: change entry page to public/login/login.html

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(express.static(path.join(__dirnameResolved, 'public')));
 
 
 app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirnameResolved, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'login', 'login.html'));
 });
 
 
